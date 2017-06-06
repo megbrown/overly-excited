37c0be52b35dee930d120f54db598202fe57b882
Megan B <brown.m.j@outlook.com>
2017-06-06 07:33:08 -0500

    finished the second part and getting there with the third

--- overly-excited.js
+++ overly-excited.js
@@ -4,41 +4,22 @@ console.log("Stacking words");
 // It should take an array containing the words of a sentence and output them in the 
 // developer console.
 
-// Example output:
-
-// The
-// The walrus
-// The walrus danced
-// The walrus danced through
-// The walrus danced through the
-// The walrus danced through the trees
-// The walrus danced through the trees in
-// The walrus danced through the trees in the
-// The walrus danced through the trees in the light
-// The walrus danced through the trees in the light of
-// The walrus danced through the trees in the light of the
-// The walrus danced through the trees in the light of the moon
-// ...
-
 // Create an array that contains the words in the sentence
 var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
-
+var stacker = [];
 // The addExcitement function should accept the array as the sole argument
 function addExcitement (theWordArray) {
-    /*
-      Write a `for` loop that iterates over the array argument and
-      outputs the words.
-     */
-     for (i=0; i < sentence.length; i++) {
-     	console.log(sentence[i]);
-     }
+     	for (i=0; i < sentence.length; i++) {
+     		stacker += sentence[i] + " ";
+     		console.log(stacker);
+     	}
 }
 
 // Invoke the function and pass in the array
 addExcitement(sentence)
 
 // Adding Conditions to the Loop
-console.log("Adding condition to the Loop");
+console.log("Adding conditions to the Loop");
 
 // Then add logic to addExcitement that places an exclamation point (!) after every 
 // third word. This will require you to do some basic math in JavaScript, 
@@ -49,20 +30,18 @@ console.log("Adding condition to the Loop");
 
 // If the current value of the counter variable can be evenly divided by 3 - using 
 // the JavaScript remainder operator - then add a single exclamation point (!) to the output.
-// Example output:
-
-// The
-// The walrus
-// The walrus danced!
-// The walrus danced! through
-// The walrus danced! through the
-// The walrus danced! through the trees!
-// The walrus danced! through the trees! in
-// The walrus danced! through the trees! in the
-// The walrus danced! through the trees! in the light!
-// The walrus danced! through the trees! in the light! of
-// The walrus danced! through the trees! in the light! of the
-// The walrus danced! through the trees! in the light! of the moon!
+var excitedStacker = [];
+function addMoreExcitement (theWordArray) {
+      for (i=0; i < sentence.length; i++) {
+        if ((i % 3) === 0 && i !== 0) {
+          excitedStacker += sentence[i] + "! ";
+        } else {
+          excitedStacker += sentence[i] + " ";
+        }
+        console.log(excitedStacker);
+      } 
+}
+addMoreExcitement(sentence)
 
 // Challenge
 console.log("Challenge");
@@ -70,22 +49,20 @@ console.log("Challenge");
 // Add logic in the addExcitement function to the increase the number of exclamation 
 // points after every third word. The number of exclamation points (!) will be 
 // determined by how many times the counter variable can be divided by 3.
+var moreExcitedStacker = [];
+function addEvenMoreExcitement (theWordArray) {
+      for (i=0; i < sentence.length; i++) {
+      if ((i % 3) === 0 && i !== 0) {
+        var numberOfExclamationPoints = i / 3;
+        moreExcitedStacker += sentence[i] + numberOfExclamationPoints + "! ";
+      } else {
+        moreExcitedStacker += sentence[i] + " ";
+      }
+      console.log(moreExcitedStacker);
+    } 
+}
+addEvenMoreExcitement(sentence)
 
-// Example output:
-
-// The
-// The walrus
-// The walrus danced!
-// The walrus danced! through
-// The walrus danced! through the
-// The walrus danced! through the trees!!
-// The walrus danced! through the trees!! in
-// The walrus danced! through the trees!! in the
-// The walrus danced! through the trees!! in the light!!!
-// The walrus danced! through the trees!! in the light!!! of
-// The walrus danced! through the trees!! in the light!!! of the
-// The walrus danced! through the trees!! in the light!!! of the moon!!!!
-// ...
 
 
 
